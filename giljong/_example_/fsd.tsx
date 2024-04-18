5778bc9c7bc81275b9295ccd262b641f14744107
devgiljong <calf-love@naver.com>
2024-04-18 20:58:19 +0900

    :construction: Fsd honey tips

---
+++ giljong/_example_/fsd.tsx
@@ -0,0 +1,15 @@
+// pages 계층
+export const LoginPage = () => null
+export const NeedLoginPage = () => null
+
+
+//app 계층
+export const withAuth = (component:any) => component
+
+
+//app 계층 | 넥스트의 경우 실제 넥스트 폴더에서 app 계층의 하이오더컴포넌트에 의존하기
+
+
+
+
+
